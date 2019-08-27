d7930b60d4c473eac233494876f04bd0bf41e28a
Arnab Das <45635614+genius-general-06@users.noreply.github.com>
2019-08-27 12:07:41 +0530

    Update Fourier Series Exp-8.py

--- Fourier Series Exp-8.py
+++ Fourier Series Exp-8.py
@@ -9,26 +9,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-#input signal
+"""input signal"""
 fs = 200
 temp = 0
 an = []
 bn = []
 x = np.linspace(0,1,fs)
-a = signal.sawtooth(4*np.pi*x,0.5)
+a = signal.sawtooth(4*np.pi*x,0.5) #generting a triangular wave with T=0.5s
 plt.subplot(2,2,1)
 plt.plot(x,a)
 
+"""coefficients"""
 for N in range(0,15,1):
-    an.append(np.trapz(a*np.cos(N*x),x))
-    bn.append(np.trapz(a*np.sin(N*x),x))
-
+    an.append(np.trapz(a*np.cos(N*x),x))#trapz for integration
+    bn.append(np.trapz(a*np.sin(N*x),x))#trapz for integration
+    
+    
+"""plotting coefficients"""
 x1 = np.linspace(0,1,len(an))
 plt.subplot(2,2,2)
 plt.stem(x1,an)
 plt.subplot(2,2,3)
 plt.stem(x1,bn)
 
+"""plotting final graph"""
 plt.subplot(2,2,4)
 for i in range(0,15,1):
     a1 = an[i]*np.cos(x*i)
