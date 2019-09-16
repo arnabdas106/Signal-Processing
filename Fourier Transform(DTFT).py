1dbd94acf170a3fc4f3ccfa04e9353172e19556c
Arnab Das <dasarnab1122@gmail.com>
2019-09-16 22:32:39 +0530

    Update Fourier Transform(DTFT).py

--- Fourier Transform(DTFT).py
+++ Fourier Transform(DTFT).py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug  5 21:23:20 2019
+Created on Mon Sep 16 22:00:02 2019
 
 @author: Arnab Das
 """
@@ -8,8 +8,8 @@ Created on Mon Aug  5 21:23:20 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 1
-
+fs = 200
+freq = 1
 ft = []
 
 a = np.arange(-10,10,1)
@@ -20,22 +20,18 @@ b[ind] = 1
 plt.subplot(2,1,1)
 plt.stem(a,b)
 
-w = np.pi/N
+w = np.pi*freq
 
-def DTFT(a):
-    a = np.asarray(a)
-    for k in range(len(a)):
-        temp = 0
-        for n in range(len(a)):
-            xn = a[n]
-            exp = np.exp(-2j*w*n*k)
-            temp += xn*exp
-            
-        ft.append(temp)
-    return ft
+"""DTFT"""
 
-result = DTFT(b)
-
+for k in range(len(b)):
+    temp = 0
+    for n in range(len(b)):
+        xn = b[n]
+        exp = np.exp(-2j*w*n*k)
+        temp += xn*exp
+    ft.append(temp)
+ 
 plt.subplot(2,1,2)
-plt.stem(a,result)
+plt.stem(a,ft)
 plt.show()
