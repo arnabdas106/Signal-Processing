9609a1d400e87b667612bb64ab814964468b9022
Arnab Das <dasarnab1122@gmail.com>
2019-09-17 10:30:15 +0530

    Add files via upload

---
+++ Freq Response Of LTI Systems exp-11.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 09:14:28 2019
+
+@author: Arnab Das
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+N = 26
+ft = []
+freq = 10
+fs = 500
+conv = []
+w = np.pi*freq
+
+"""H(jw)"""
+a = np.linspace(0,2*np.pi,N)
+b = np.zeros(len(a))
+ind1 = np.where(a>np.pi/2)
+b[ind1] = 10
+ind2 = np.where(a>3*np.pi/2)
+b[ind2] = 0
+
+plt.subplot(4,1,1)
+plt.stem(a,b)
+
+"""IFT"""
+for t in range(0,len(a),1):
+    sum=0
+    w=a[t]
+    for i in range(0,25,1):
+        sum=sum+(((b[i])*np.complex(np.cos(w*i),np.sin(w*i))))
+    ft.append(sum.real)
+
+
+a1 = np.linspace(0,26,26)
+plt.subplot(4,1,2)
+plt.stem(a1,ft)
+
+"""Input Signal"""
+a2 = np.linspace(0,1,fs)
+b2 = np.sin(2*np.pi*freq*a2)
+plt.subplot(4,1,3)
+plt.plot(a2,b2)
+
+
+"""Convolution"""
+l1 = len(ft)
+l2 = len(b2)
+No = l1+l2-1
+conv = np.convolve(ft,b2)
+
+t = np.linspace(0,1,No)
+plt.subplot(4,1,4)
+plt.stem(t,conv)
+plt.show()
